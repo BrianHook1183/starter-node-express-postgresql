b2e2a943f463d75138b7fe4935bcd1ffccc2400c
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-06-02 17:19:59 -0500

    runs migration on server start

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
 const { PORT = 5000 } = process.env;
 
 const app = require("./app");
+const knex = require("./db/connection");
+
 const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+
+// The server now imports Knex and calls knex.migrate.latest(), which applies all migrations that have not yet been applied and returns a promise containing the migrations that were applied, if any. If the migration is successful, the app is started on the specified port. Otherwise, the error is printed to console.error() and then explicitly tears down the connection to the database by calling knex.destroy().
+
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT, listener);
+  })
+  .catch((error) => {
+    console.error(error);
+    knex.destroy();
+  });
